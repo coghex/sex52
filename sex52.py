47e0b515434423f94de8283fa5d35db2ee945d0f
coghex <vincecoghlan@gmail.com>
2021-10-01 10:12:54 -0700

    ctrl-c handling

--- sex52.py
+++ sex52.py
@@ -1,23 +1,37 @@
 import evdev
+import signal
 from x52_driver import X52Driver, X52ProEvdevKeyMapping, X52MfdLine, X52ColoredLedStatus
+
+def main():
+    for event in device.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            if (event.code == X52ProEvdevKeyMapping.FIRE_C):
+                if (event.value == evdev.events.KeyEvent.key_down):
+                    dev.set_mfd_text(X52MfdLine.LINE2,"blop blop")
+            #if (eventLEDButton(event.code)):
+            if (event.code == X52ProEvdevKeyMapping.FIRE_A):
+                if (event.value == evdev.events.KeyEvent.key_down):
+                    dev.set_led_a(X52ColoredLedStatus.RED)
+                if (event.value == evdev.events.KeyEvent.key_up):
+                    dev.set_led_a(X52ColoredLedStatus.GREEN)
+#            try:
+#                print(event.code)
+#            except KeyError:
+#                print("keyerror")
+
+def eventLEDButton(eventval):
+    eventval == X52ProEvdevKeyMapping.FIRE_A
 
 device = evdev.InputDevice('/dev/input/by-id/usb-Logitech_X52_Professional_H.O.T.A.S.-event-joystick')
 dev    = X52Driver.find_supported_devices()[0]
-
 #print(device.capabilities(verbose=True))
 #print(device.input_props(verbose=True))
 
-for event in device.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        if (event.code == X52ProEvdevKeyMapping.FIRE_C):
-            if (event.value == evdev.events.KeyEvent.key_down):
-                dev.set_mfd_text(X52MfdLine.LINE2,"blop blop")
-        if (event.code == X52ProEvdevKeyMapping.FIRE_A):
-            if (event.value == evdev.events.KeyEvent.key_down):
-                dev.set_led_a(X52ColoredLedStatus.RED)
-            if (event.value == evdev.events.KeyEvent.key_up):
-                dev.set_led_a(X52ColoredLedStatus.GREEN)
-#        try:
-#            print(event.code)
-#        except KeyError:
-#            print("keyerror")
+def sigInt(sig_in,frame):
+    print("bye...")
+    device.close()
+    exit(0)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, sigInt)
+    main()
