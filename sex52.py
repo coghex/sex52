18005129bd9a3ec47f3621b30007141c1fee19e4
coghex <vincecoghlan@gmail.com>
2021-10-01 09:44:41 -0700

    a few examples

--- sex52.py
+++ sex52.py
@@ -1,5 +1,5 @@
 import evdev
-from x52_driver import X52Driver, X52ProEvdevKeyMapping, X52MfdLine
+from x52_driver import X52Driver, X52ProEvdevKeyMapping, X52MfdLine, X52ColoredLedStatus
 
 device = evdev.InputDevice('/dev/input/by-id/usb-Logitech_X52_Professional_H.O.T.A.S.-event-joystick')
 dev    = X52Driver.find_supported_devices()[0]
@@ -12,6 +12,11 @@ for event in device.read_loop():
         if (event.code == X52ProEvdevKeyMapping.FIRE_C):
             if (event.value == evdev.events.KeyEvent.key_down):
                 X52Driver.set_mfd_text(dev,X52MfdLine.LINE2,"blop blop")
+        if (event.code == X52ProEvdevKeyMapping.FIRE_A):
+            if (event.value == evdev.events.KeyEvent.key_down):
+                X52Driver.set_led_a(dev,X52ColoredLedStatus.RED)
+            if (event.value == evdev.events.KeyEvent.key_up):
+                X52Driver.set_led_a(dev,X52ColoredLedStatus.GREEN)
 #        try:
 #            print(event.code)
 #        except KeyError:
